7cb5fc988d9d2842d646b46e1b912b836c7fcbed
kihiuFrank <franklinekihiu@gmail.com>
2023-01-12 07:39:56 +0300

    showing decks on the landing page

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import Deck from "./models/Deck";
 import { config } from "dotenv";
+import cors from "cors";
 
 config();
 
@@ -9,8 +10,23 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.post("/deck", async (req: Request, res: Response) => {
+app.get("/decks", async (req: Request, res: Response) => {
+  // fetch all decks and send bakc to the user
+  // 1. fetch the decks from mongo
+  const decks = await Deck.find();
+  console.log(decks);
+
+  // 2. send back the array to the UI
+  res.json(decks);
+});
+
+app.post("/decks", async (req: Request, res: Response) => {
   const newDeck = new Deck({
     title: req.body.title,
   });
